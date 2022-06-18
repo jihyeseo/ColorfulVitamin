c9d34e776530924bfb87471c558f855d0ca6fe3b
jihyeseo <jihyeseo@gmail.com>
2022-06-18 12:44:26 +0200

    Refresh count daily, to archive and start anew from 0 every day

--- app/src/main/java/com/example/colorfulvitamin/RecordFragment.kt
+++ app/src/main/java/com/example/colorfulvitamin/RecordFragment.kt
@@ -1,5 +1,7 @@
 package com.example.colorfulvitamin
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.colorfulvitamin.databinding.FragmentRecordBinding
+import com.google.android.material.snackbar.Snackbar
+import java.time.LocalDate
+import java.time.LocalDateTime
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -32,13 +37,62 @@ class RecordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
         binding.buttonHistory.setOnClickListener {
             findNavController().navigate(R.id.action_RecordFragment_to_HistoryFragment)
+        }
+
+        binding.buttonRed.setOnClickListener {
+            recordAndNotify(it, "red")
+        }
+
+        binding.buttonGreen.setOnClickListener {
+            recordAndNotify(it, "green")
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun cleanDaily() {
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val timestampKey = "lastRecordAt"
+        val lastRecordedAt = sharedPref.getString(timestampKey, "")
+        val currentTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LocalDate.now().toString()
+        } else {
+            "" // TODO("VERSION.SDK_INT < O")
+        }
+        val firstRecordOfDay = lastRecordedAt != currentTime
+        if (!firstRecordOfDay) {
+            return
+        }
+        val keyNames = arrayOf("red", "green")
+        // Move all data to previous day data.
+        with (sharedPref.edit()) {
+            for (key in keyNames) {
+                val yesterdayValue = sharedPref.getInt(key, 0)
+                putInt(key+lastRecordedAt, yesterdayValue)
+                putInt(key, 0)
+            }
+            putString(timestampKey, currentTime)
+            apply()
+        }
+    }
+
+    fun recordAndNotify(view: View, key: String) {
+        cleanDaily()
+
+        val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+        val prevValue = sharedPref.getInt(key, 0)
+        with (sharedPref.edit()) {
+            putInt(key, 1 + prevValue)
+            apply()
+        }
+        val mySnackbar = Snackbar.make(view, "$prevValue $key", Snackbar.LENGTH_SHORT)
+        mySnackbar.show()
     }
 }
